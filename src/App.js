d5b93738bd98571dad56ceda94940fde7c1ef81c
Andrew Perkins <Andrew.s.perkins7.ctr@mail.mil>
2024-04-29 12:36:45 -0400

    counter app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const savedCount = parseInt(localStorage.getItem('count'), 10);
+    if (!isNaN(savedCount)) {
+      setCount(savedCount);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('count', count.toString());
+  }, [count]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((prevCount) => prevCount + 1);
+    }, 86400000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const resetCount = () => {
+    setCount(0);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Days since last Trevor/Treynor Incident</h1>
+        <p>Count: {count}</p>
+        <button onClick={resetCount}>Reset</button>
       </header>
     </div>
   );
